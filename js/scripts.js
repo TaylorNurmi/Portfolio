a7cc28a8583607d7715f01e0c026b4703e644e5d
Taylor Nurmi <71565003+TaylorNurmi@users.noreply.github.com>
2022-10-16 19:10:07 -0600

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,86 @@
+window.addEventListener('DOMContentLoaded', event => {
+
+    const sidebarWrapper = document.getElementById('sidebar-wrapper');
+    let scrollToTopVisible = false;
+    // Closes the sidebar menu
+    const menuToggle = document.body.querySelector('.menu-toggle');
+    menuToggle.addEventListener('click', event => {
+        event.preventDefault();
+        sidebarWrapper.classList.toggle('active');
+        _toggleMenuIcon();
+        menuToggle.classList.toggle('active');
+    })
+
+    // Closes responsive menu when a scroll trigger link is clicked
+    var scrollTriggerList = [].slice.call(document.querySelectorAll('#sidebar-wrapper .js-scroll-trigger'));
+    scrollTriggerList.map(scrollTrigger => {
+        scrollTrigger.addEventListener('click', () => {
+            sidebarWrapper.classList.remove('active');
+            menuToggle.classList.remove('active');
+            _toggleMenuIcon();
+        })
+    });
+
+    function _toggleMenuIcon() {
+        const menuToggleBars = document.body.querySelector('.menu-toggle > .fa-bars');
+        const menuToggleTimes = document.body.querySelector('.menu-toggle > .fa-xmark');
+        if (menuToggleBars) {
+            menuToggleBars.classList.remove('fa-bars');
+            menuToggleBars.classList.add('fa-xmark');
+        }
+        if (menuToggleTimes) {
+            menuToggleTimes.classList.remove('fa-xmark');
+            menuToggleTimes.classList.add('fa-bars');
+        }
+    }
+
+    // Scroll to top button appear
+    document.addEventListener('scroll', () => {
+        const scrollToTop = document.body.querySelector('.scroll-to-top');
+        if (document.documentElement.scrollTop > 100) {
+            if (!scrollToTopVisible) {
+                fadeIn(scrollToTop);
+                scrollToTopVisible = true;
+            }
+        } else {
+            if (scrollToTopVisible) {
+                fadeOut(scrollToTop);
+                scrollToTopVisible = false;
+            }
+        }
+    })
+})
+
+function fadeOut(el) {
+    el.style.opacity = 1;
+    (function fade() {
+        if ((el.style.opacity -= .1) < 0) {
+            el.style.display = "none";
+        } else {
+            requestAnimationFrame(fade);
+        }
+    })();
+};
+
+function fadeIn(el, display) {
+    el.style.opacity = 0;
+    el.style.display = display || "block";
+    (function fade() {
+        var val = parseFloat(el.style.opacity);
+        if (!((val += .1) > 1)) {
+            el.style.opacity = val;
+            requestAnimationFrame(fade);
+        }
+    })();
+};
+
+function contactOn() {
+    var divToChange = document.getElementById("reachme");
+    divToChange.innerHTML ='<h2><u>Contact Me</u></h2> <h3>EMAIL</h3> <a class="listentry" href="mailto:tnurmi0714@gmail.com">tnurmi0714@gmail.com</a> <h3>GITHUB</h3> <a class="listentry" href="https://github.com/TaylorNurmi">github.com/TaylorNurmi</a> <h3>LINKEDIN</h3> <a class="listentry" href="https://www.linkedin.com/in/taylor-nurmi/">linkedin.com/in/taylor-nurmi/</a>';
+}
+function softOn() {
+    var divToChange = document.getElementById("reachme");
+    divToChange.innerHTML = '<h2><u>Soft Skills</u></h2> <h3>Leadership</h3> <h3>Communication</h3> <h3>Problem Solving</h3> <h3>Teamwork</h3> <h3>Microsoft Office</h3>'
+}
+
+
